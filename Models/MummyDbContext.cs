95fb88b8801830dc296bea6f4bf7126abdd4ca02
matthiashulme <matthiashulme@gmail.com>
2023-04-10 17:03:46 -0600

    DBContext changes

---
+++ Models/MummyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Intex_group1_8.Models
+{
+	public class MummyDbContext : DbContext
+	{
+		public MummyDbContext()
+		{
+		}
+	}
+}
+
