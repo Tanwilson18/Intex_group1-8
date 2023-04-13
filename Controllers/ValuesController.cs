78bab3c4b5b7a9cb3e28619dcbb31d85c3c564ac
Caleb Hefner <hefnerca5@gmail.com>
2023-04-13 16:21:03 -0600

    Adding the predict frontend

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -28,9 +28,13 @@ namespace Intex_group1_8.Controllers
                 NamedOnnxValue.CreateFromTensor("float_input", data.AsTensor())
             });
             Tensor<string> predict = result.First().AsTensor<string>();
-            var categories = new[] { "H", "S", "W", "B" };
+            var categories = new[] { "B", "H", "S", "W" };
             int predictionIndex = Array.IndexOf(predict.ToArray(), predict.Max());
             var prediction = new Prediction { PredictedValue = categories[predictionIndex] };//max score different. Get just categorical value
+
+            //Tensor<string> predict = result.First().AsTensor<string>();
+            //string predictionValue = predict.ToArray()[0];
+            //var prediction = new Prediction { PredictedValue = predictionValue };
             return Ok(prediction);
         }
     }
